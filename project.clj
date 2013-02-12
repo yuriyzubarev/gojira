4cded8b841580fd9e9b98ad1c4a852ceecb148dc
Yuriy Zubarev <yzubarev@dusssw1.abebooks.com>
2013-02-11 21:05:24 -0800

    first functional prototype

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject gojira "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :main gojira.core
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+    [org.clojure/clojure "1.4.0"]
+    [clj-http "0.6.4"]
+    [org.clojure/tools.cli "0.2.2"]])
---
+++ src/gojira/core.clj
@@ -0,0 +1,108 @@
+(ns gojira.core
+   (:require 
+    [clj-http.client :as client]
+    [clojure.walk :as w]
+    [clojure.tools.cli :as cli]
+    [clojure.string :as s]))
+
+; globals for a CLI tool: evil or pragmatic?
+(def jira-api-url)
+(def username)
+(def password)
+(def sprint-id)
+
+(defn select-values [map ks]
+  "From http://blog.jayfields.com/2011/01/clojure-select-keys-select-values-and.html"
+  (remove nil? (reduce #(conj %1 (map %2)) [] ks)))
+
+(defn jira-api-call [url query-params]
+  (client/get (if (= "http" (apply str (take 4 url))) url (str jira-api-url "/" url))
+    {:basic-auth    [username password]
+     :content-type  :json
+     :insecure?     true
+     :query-params  query-params
+     :as            :json}))
+
+(defn search-result []
+  (:body (jira-api-call "search" {:jql (str "sprint = " sprint-id " AND issuetype in standardIssueTypes()")})))
+
+(defn extract-epic-data [issue-number]
+  (:summary (:fields (first (:issues (:body (jira-api-call "search" {:jql (str "issue = " issue-number)})))))))
+
+(defn extract-story-points [issue]
+  (int (or (:customfield_10243 (:fields issue)) 1)))
+
+(defn extract-status [issue]
+  (:name (:status (:fields issue))))
+
+(defn extract-changelog [issue-url]
+    (let [histories (:histories (:changelog (:body (jira-api-call issue-url {:expand "changelog"}))))]
+        (let [items (map vector (w/walk #(:items %) seq histories) (map #(:created %) histories))]
+          (let [status-items (filter #(= "status" (:field (ffirst %))) items)]
+            (map #(into {} { :from (:fromString (ffirst %)) :to (:toString (ffirst %)) :date (last %) }) status-items)))))
+
+(defn extract-search-issue-data [issue]
+    "Issue in json from 'search' command"
+    {
+      :self (:self issue)
+      :status (extract-status issue)
+      :points (extract-story-points issue)
+      :epic-name (extract-epic-data (:customfield_11180 (:fields issue)))
+      :summary (str (:key issue) " " (:summary (:fields issue))) })
+
+(defn sprint-snapshot []
+    (map extract-search-issue-data (:issues (search-result))))
+
+(defn format-sprint-snapshot [l]
+    (map #(select-values % [:self :status :points :epic-name :summary]) l))
+
+(defn print-sprint-snapshot [l]
+    (dorun (map println (map #(s/join "\t" %) (format-sprint-snapshot l)))))
+
+(defn sprint-flow []
+    (map #(assoc % :changelog (extract-changelog (:self %))) (sprint-snapshot)))
+
+(defn p-flow [l]
+  (let [[changelog sprint] (take 2 l)]
+    (cond
+      (nil? changelog) '()
+      :else
+        (do
+          (print-sprint-snapshot (list changelog))
+          ; (println (:changelog changelog))
+          (dorun (map #(println (s/join "\t" %)) (map #(select-values % [:from :to :date]) (:changelog changelog))))
+          (p-flow (drop 2 l))))))
+
+(defn print-sprint-flow [l]
+    (let [flow (interleave l (format-sprint-snapshot l))]
+      (p-flow flow)))
+      ; (println flow)))
+    ; (dorun (map println (map #(s/join "\t" %) flow)))))
+
+(defn -main [& args]
+  (let [[opts args banner]
+        (cli/cli args
+             ["-h" "--help" "Show help" :flag true :default false]
+             ["-u" "--user" "Username" :default "yzubarev"]
+             ["-p" "--password" "Password"]
+             ["-s" "--sprint" "Sprint ID"]
+             ["-j" "--jira-api-url" "JIRA API URL" :default "https://jira.kokanee.abebooks.com/jira/rest/api/2"]
+             )]
+    (when (:help opts)
+      (println banner)
+      (System/exit 0))
+    (if
+        (and
+         (:password opts)
+         (:sprint opts))
+      (do
+        (def jira-api-url (:jira-api-url opts))
+        (def username (:user opts))
+        (def password (:password opts))
+        (def sprint-id (:sprint opts))
+        (cond
+          (= "snapshot" (first args)) (print-sprint-snapshot (sprint-snapshot))
+          (= "flow" (first args))     (print-sprint-flow (sprint-flow))
+          :else (println "Nothing to do")))
+      (println banner))))
+  

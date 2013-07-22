599082f43fbf784cc8f5406f3cc45a63e987f48b
Yuriy Zubarev <yzubarev@sphere.kokanee.abebooks.com>
2013-07-22 10:44:52 -0700

    error checking

--- mail.sh
+++ mail.sh
@@ -2,6 +2,12 @@ lein compile
 rm .tmp-*
 lein run -u "$1" -p "$2" -s $4 -j "$5" by-status > daily.html 
 
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then
+
+echo "" | mail -s "Jira daily report wasn't sent" yzubarev@abebooks.com
+
+else
+
 cat - daily.html <<HERE | sendmail -oi -t
 From: $1@abebooks.com
 To: $3
@@ -11,3 +17,5 @@ Content-Transfer-Encoding: 7bit
 MIME-Version: 1.0
 
 HERE
+
+fi
